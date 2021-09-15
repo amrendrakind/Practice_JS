c81eaf69acf200df8fc418a0ea6f52565b85322c
amrendrakind <vision2010@gmail.com>
2021-09-15 21:11:15 +0530

    Start..

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-parseInt(Animatio)
+var testObject={
+    "an atom" : "Amrendra K",
+    "my side" : "Nilesh",
+    "My drink" : "Water"
+}
+
+var atomValue = testObject["My drink"]
+console.log(atomValue);
+atomValue = testObject["My drink"].length
+console.log(atomValue);
