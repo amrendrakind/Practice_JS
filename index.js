ef1013a7a164df9f38a15be97a2a549728d69bd0
amrendrakind <vision2010@gmail.com>
2021-09-15 23:20:39 +0530

    Start..

--- index.js
+++ index.js
@@ -28,3 +28,18 @@ console.log(atomValue);
 console.log(testObject)
 delete testObject.add
 console.log(testObject)
+
+function phoneticLookup(val){
+var result="";
+var lookup ={
+    "alpha" : "Adams",
+    "bravo" : "Boston",
+    "charlie" :"Chicago",
+    "delta" : "Denver",
+    "echo" : "Easy",
+    "foxhort" : "frank"
+}
+result = lookup[val]
+return result;
+}
+console.log(phoneticLookup("delta"))
