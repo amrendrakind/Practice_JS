d5c0e8dfba382eaf55a93fdccb19ecb50ea49ea5
amrendrakind <vision2010@gmail.com>
2021-09-16 12:57:55 +0530

    Questions.

--- index.js
+++ index.js
@@ -10,3 +10,14 @@ function fib(n) {
     return a;
     }
 console.log(fib(20))
+//
+
+var s = "hello";
+
+console.log( s.substr(1,4) == "ello" )
+console.log(( s.substring(1,4) == "ell" ))
+
+var date = new Date();
+var n = date.toISOString();
+console.log(n);
+
