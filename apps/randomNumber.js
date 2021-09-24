a8e7591788a8546832dd553cbfdfce23b19b4b1b
amrendrakind <vision2010@gmail.com>
2021-09-24 22:37:51 +0530

    randomNumber

--- apps/randomNumber.js
+++ apps/randomNumber.js
@@ -1,4 +1,12 @@
+//Any random number between 1 to max
 function randomNumber(max){
     return Math.floor(Math.random()*max)
 }
 console.log(randomNumber(20));
+
+//any random numnber between max and min
+
+function randomNumberBetween(min, max){
+    return (Math.floor(Math.random()*(max-min+1)+min))
+}
+console.log(randomNumberBetween(10,20));
