783df6fe5ac3f7dec9caa3a25fb130e3f6928d69
amrendrakind <vision2010@gmail.com>
2021-09-27 13:49:28 +0530

    Working with Objects

--- apps/index.js
+++ apps/index.js
@@ -1,26 +1,10 @@
-let obj={
-    name:"John Snow",
-    watchMember: "true"
-}
-console.log(obj)
-obj.gamesOfThrones = "awesome"
-console.log(obj)
-delete obj.gamesOfThrones
-console.log(obj)
-//Excercise----
-
 let myObj = {
     firstName : "Amrendra",
     lastName: "K",
     occupation: "Software Engineer"
 }
-console.log(myObj.firstName)
-console.log(myObj.lastName)
-console.log(myObj.occupation)
 myObj.hobby = "Playing Kite"
 console.log(myObj)
-delete myObj.occupation
-console.log(myObj)
-console.log(myObj["firstName"])
-console.log(myObj["lastName"])
-console.log(myObj["hobby"])
+for (let value in myObj){
+    console.log(myObj[value])
+}
