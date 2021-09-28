d2a9d26a1f6f0fcb6eab933b8a26778df0d3e79f
amrendrakind <vision2010@gmail.com>
2021-09-28 15:44:35 +0530

    Immediately Invoked Function Expressions

--- apps/index.js
+++ apps/index.js
@@ -33,5 +33,33 @@ let personObj = (function invokeRightAway(){
 //console.log(personObj)
 console.log(personObj.getName())
 personObj.setName("Amrendra Kumar");
-console.log(personObj.getName())
+console.log(personObj.getName());
 
+//Write a function called displayFullName, which should accept two parameters, firstName and lastName. The function should be immediately invoked and return the firstName + lastName. You should NOT have to call this function, it should invoke right away.
+firstName1="Akshaj"
+lastName1="K"
+let fullName = (function displayFullName(firstName, lastName){
+    return (firstName+" "+lastName)
+})(firstName1, lastName1)
+console.log(fullName)
+//Write a function called createCalculator, which should return an object that has four methods, add, subtract, multiply and divide.
+let calculator=(function createCalculator(){
+    return{
+        add : function(a,b){
+            return (a+b)
+        },
+        substract: function(a,b){
+            return (a-b)
+        },
+        multiply: function(a,b){
+            return (a*b)
+        },
+        divide: function(a,b){
+            return (a/b)
+        }
+    }
+})()
+console.log(calculator.add(10,5))
+console.log(calculator.substract(10,5))
+console.log(calculator.multiply(10,5))
+console.log(calculator.divide(10,5))
