6a5a728b2ae3b81bc5faed9a600de906682f1a27
amrendrakind <vision2010@gmail.com>
2021-09-29 00:01:49 +0530

    Functions Exercises

--- apps/index.js
+++ apps/index.js
@@ -4,3 +4,48 @@ function difference(a,b){
 }
 console.log(difference(2,2)) //0
 console.log(difference(0,2)) //-2
+
+//This function takes in two parameters and returns the product of the two E.g product(2,2); // 4
+
+function product(a,b){
+    return (a*b)
+}
+console.log(product(2,2)) //4
+console.log(product(0,2)) //0
+
+//printDay - This function takes in one parameter (a number from 1-7) and returns the day of the week (1 is Sunday, 2 is Monday, 3 is Tuesday etc.). If the number is less than 1 or greater than 7, the function should return undefined;
+function printDay(day){
+    let dayStr=''
+    switch (day) {
+        case 1: 
+            dayStr = "Sunday"
+            break;
+        case 2:
+            dayStr = "Monday"
+            break;
+        case 3:
+            dayStr = "Tuesday"
+            break;
+        case 4:
+            dayStr = "Wednesday"
+            break;
+        case 5:
+            dayStr = "Thursday"
+            break;
+        case 6:
+            dayStr = "Friday"
+            break;
+        case 7:
+            dayStr = "Saturday"
+            break;
+        default:
+            dayStr = "undefined"
+            break;
+    }
+    return dayStr;
+}
+console.log(printDay(1))    //Sunday
+console.log(printDay(3))    //Tuesday
+console.log(printDay(5))    //THursday
+console.log(printDay(7))    //Saturday
+console.log(printDay(41))   //undefined
