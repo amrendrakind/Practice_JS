da9e2ce691bff0f3ab59284da6d0436fc7032936
amrendrakind <vision2010@gmail.com>
2021-09-28 14:30:22 +0530

    Immediately Invoked Function Expressions

--- apps/iifeFunction.js
+++ apps/iifeFunction.js
@@ -3,7 +3,7 @@ let result=(function(){
     return person;
 })();
 console.log(result)
-//
+//IIFE returns Objects
 let personObject= (function(){
     return{
         name :"Amrendra",
@@ -17,4 +17,21 @@ console.log(personObject.age)
 console.log(personObject.occupation)
 console.log(personObject.hobbies)
 console.log("\n",personObject)
+// IIFE object with functions for the values of the keys:
+
+let personObj = (function invokeRightAway(){
+    let person = "Amrendra"
+    return{
+        getName : function(){
+            return person
+        },
+        setName : function(newName){
+            person = newName
+        }
+    }
+})()
+//console.log(personObj)
+console.log(personObj.getName())
+personObj.setName("Amrendra Kumar");
+console.log(personObj.getName())
 
