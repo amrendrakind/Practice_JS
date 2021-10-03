2cbde848ef0323ca4bbd062b5262b2fb05f0da8d
amrendrakind <vision2010@gmail.com>
2021-10-03 16:47:19 +0530

    Nested Array Exercises

--- apps/index.js
+++ apps/index.js
@@ -1,13 +1,13 @@
-// let myFirstSubarray = [["this", "is"], ["super", "cool"]];
-// for(let myArray of myFirstSubarray){
-//   console.log(myArray)
-// }
-// //----------------------
-// for(let nestedArray of myFirstSubarray){
-//   for(let myArray of nestedArray){
-//     console.log(myArray)
-//   }
-// }
+let myFirstSubarray = [["this", "is"], ["super", "cool"]];
+for(let myArray of myFirstSubarray){
+  console.log(myArray)
+}
+//----------------------
+for(let nestedArray of myFirstSubarray){
+  for(let myArray of nestedArray){
+    console.log(myArray)
+  }
+}
 let nestedArr = [[1, 2],
                  [3, 4]
                 ]
---
+++ apps/nestedArrayExercises.js
@@ -0,0 +1,18 @@
+let myFirstSubarray = [["this", "is"], ["super", "cool"]];
+for(let myArray of myFirstSubarray){
+  console.log(myArray)
+}
+//----------------------
+for(let nestedArray of myFirstSubarray){
+  for(let myArray of nestedArray){
+    console.log(myArray)
+  }
+}
+let nestedArr = [[1, 2],
+                 [3, 4]
+                ]
+for(let i =0;i<nestedArr.length;i++){
+  for(let j=0;j<nestedArr.length;j++){
+    console.log(nestedArr[j][i])
+  }
+}
