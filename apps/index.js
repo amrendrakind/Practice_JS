ee11c0b437a67ade1bfc6f7ad8d9b9300fe33793
amrendrakind <vision2010@gmail.com>
2021-10-03 19:16:05 +0530

    Nested Array Exercises

--- apps/index.js
+++ apps/index.js
@@ -12,7 +12,42 @@ let nestedArr = [[1, 2],
                  [3, 4]
                 ]
 for(let i =0;i<nestedArr.length;i++){
-  for(let j=0;j<nestedArr.length;j++){
+  for(let j=0;j<nestedArr[i].length;j++){
     console.log(nestedArr[j][i])
   }
 }
+let nestedArr1 = [[1, 2, 3], 
+                  [4, 5, 6], 
+                  [7, 8, 9, 10, 11, 12]];
+for(let i=0;i<nestedArr1.length;i++){
+  for(let j=0;j<nestedArr1[i].length;j++){
+    console.log(nestedArr1[i][j])
+  }
+}
+//--printEvens
+console.log("-------------Print Even Numbers----------------")
+let nestedArr2 = [[1, 2, 3], [4, 5, 6], [7, 8], [9, 10, 11, 12]];
+function printEvens(nestedArray){
+  let evenArray=[]
+  for(let arrayElement of nestedArray){
+    for(let element of arrayElement){
+      if(element%2===0){
+        evenArray.push(element)
+      }
+    }
+  }
+  return evenArray
+}
+console.log(printEvens(nestedArr2))
+console.log("-------------Sum Total of Array Elements----------------")
+function sumTotal(nestedArray){
+  let sumOfElements=0
+  for(let arrayElement of nestedArray){
+    for(let element of arrayElement){
+      sumOfElements+=element
+    }
+  }
+  return sumOfElements
+}
+console.log(sumTotal(nestedArr2))
+
