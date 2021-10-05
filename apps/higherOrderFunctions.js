a9c6c9c104ad1c20b0962ebe7fea931d7385313e
amrendrakind <vision2010@gmail.com>
2021-10-05 23:08:16 +0530

    Higher Order Functions

--- apps/higherOrderFunctions.js
+++ apps/higherOrderFunctions.js
@@ -1,6 +1,17 @@
 function sendMessage(message, fn){
     return fn(message);
   }
-  
   sendMessage("Hello World", console.log); // Hello World
+  // Another One
   
+  function add(a,b){
+    return a+b
+  }
+  function substract(a,b){
+    return a-b
+  }
+  function math(a,b,calllBack){
+    return calllBack(a,b)
+  }
+  console.log(math(1,4,add))
+  console.log(math(8,5,substract))
