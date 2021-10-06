58606d9149009bdfc11972acf3d31113dbb089fb
amrendrakind <vision2010@gmail.com>
2021-10-06 23:47:51 +0530

    Higher Order Functions

--- apps/higherOrderFunctions.js
+++ apps/higherOrderFunctions.js
@@ -1,17 +1,38 @@
 function sendMessage(message, fn){
-    return fn(message);
+  return fn(message);
+}
+sendMessage("Hello World", console.log); // Hello World
+// Another One callBack
+
+function add(a,b){
+  return a+b
+}
+function substract(a,b){
+  return a-b
+}
+function math(a,b,calllBack){
+  return calllBack(a,b)
+}
+console.log(math(1,4,add))
+console.log(math(8,5,substract))
+//Next
+console.log("Higher Order function")
+function each(arr, fn){
+  for(let i = 0; i<arr.length;i++){
+    fn(arr[i])
   }
-  sendMessage("Hello World", console.log); // Hello World
-  // Another One
-  
-  function add(a,b){
-    return a+b
-  }
-  function substract(a,b){
-    return a-b
-  }
-  function math(a,b,calllBack){
-    return calllBack(a,b)
-  }
-  console.log(math(1,4,add))
-  console.log(math(8,5,substract))
+}
+each([1,2,3,4,5],function(val){console.log(val)})
+console.log("Double Number")
+each([1,2,3,4,5],function(val){console.log(val*2)})
+//-----------MAP Function
+//Write a function called map which accepts two parameters: an array and a callback. The map function should return a new array with the result of each value being passed to the callback function.
+
+function map(arr,fn){
+    let tempArr=[]
+    for(let i =0; i<arr.length;i++){
+      tempArr.push(fn(arr[i]))
+    }
+    return tempArr
+}
+console.log(map([6,7,8,9,10],function (val){return val*2}))
--- apps/index.js
+++ apps/index.js
@@ -2,7 +2,7 @@ function sendMessage(message, fn){
   return fn(message);
 }
 sendMessage("Hello World", console.log); // Hello World
-// Another One
+// Another One callBack
 
 function add(a,b){
   return a+b
@@ -15,3 +15,24 @@ function math(a,b,calllBack){
 }
 console.log(math(1,4,add))
 console.log(math(8,5,substract))
+//Next
+console.log("Higher Order function")
+function each(arr, fn){
+  for(let i = 0; i<arr.length;i++){
+    fn(arr[i])
+  }
+}
+each([1,2,3,4,5],function(val){console.log(val)})
+console.log("Double Number")
+each([1,2,3,4,5],function(val){console.log(val*2)})
+//-----------MAP Function
+//Write a function called map which accepts two parameters: an array and a callback. The map function should return a new array with the result of each value being passed to the callback function.
+
+function map(arr,fn){
+    let tempArr=[]
+    for(let i =0; i<arr.length;i++){
+      tempArr.push(fn(arr[i]))
+    }
+    return tempArr
+}
+console.log(map([6,7,8,9,10],function (val){return val*2}))
