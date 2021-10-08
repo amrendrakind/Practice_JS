949a059de9d4a2d571a1b5e3526c899f983f1845
amrendrakind <vision2010@gmail.com>
2021-10-08 17:12:52 +0530

    Closures

--- apps/index.js
+++ apps/index.js
@@ -30,6 +30,11 @@ console.log("Now age is",ageGrowByOne()) // 29
 console.log("Now age is",ageGrowByOne()) // 30
 console.log("Now age is",ageGrowByOne()) // 31
 console.log("Now age is",ageGrowByOne()) // 32
+
+let ageGrowBytwo = defineAge();
+console.log("Now age by 2 is ",ageGrowBytwo())
+console.log("Now age by 2 is ",ageGrowBytwo())
+
 /// Another Example-----------
 function createInstructors(){
   let instructors=["Elie","Matt","Tim"]
@@ -113,3 +118,25 @@ function add(){
 console.log(add(1,2,3,4,5,6))
 console.log(add(-1,1,2,-3,-4))
 console.log(add())
+/// Write a function called createCounter. This function should contain a variable count that is initialized to 0. This function should return another function that when invoked, increments the counter by 1 and returns the count variable. You should be able to create multiple counters with the createCounter function and they should all have their own private variable called count.
+
+function createCounter(){
+  let count=0
+  return function counter(){
+    return ++count
+  }
+}
+let firstCounter = createCounter()
+console.log(firstCounter())
+console.log(firstCounter())
+console.log(firstCounter())
+console.log(firstCounter())
+
+let secondCounter=createCounter()
+
+console.log("from 2nd" ,secondCounter())
+console.log("from 2nd" ,secondCounter())
+
+console.log(firstCounter())
+console.log(firstCounter())
+console.log(firstCounter())
