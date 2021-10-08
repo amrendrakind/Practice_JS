d149e5cd5af42c2db72e8f4063fc0292c823d2c1
amrendrakind <vision2010@gmail.com>
2021-10-08 19:36:51 +0530

    Closures Exercise

--- apps/closureExercise.js
+++ apps/closureExercise.js
@@ -2,13 +2,14 @@
 //Write a function called countdown that accepts a number as a parameter and every 1000 milliseconds decrements the value and console.logs it. Once the value is 0 it should log "DONE!" and stop.
 
 function countdown(number){
-  var timer=setInterval(function(){
-    number--
-    if(number<1){
-      clearInterval(timer)
-      console.log("DONE!")
-    }
-    else{console.log(number)}
-  },1000)
-}
-countdown(5)
+    var timer=setInterval(function(){
+      number--
+      if(number<1){
+        clearInterval(timer)
+        console.log("DONE!")
+      }
+      else{console.log(number)}
+    },1000)
+  }
+  countdown(5)
+  
