2b9c7f9fdf122876fc3c5b9bbebe32a9effc5e11
amrendrakind <vision2010@gmail.com>
2021-10-08 14:14:04 +0530

    Closures

--- apps/index.js
+++ apps/index.js
@@ -1,5 +1,16 @@
-console.log("first");
-setTimeout(function(){
-    console.log("second");
-},1000);
-console.log("third");
+function outer(a){
+  return function inner(b){
+      return a + b;
+  }
+}
+
+console.log(outer(10)) // this returns the inner function
+
+// this calls the inner function right away
+console.log(outer(5)(2)) // 7
+
+// // we can store the inner function in a variable
+let laterAdd = outer(10)
+// we can now call that inner function
+console.log(laterAdd(15)) // 25
+console.log(outer(25)(10)) // 35
