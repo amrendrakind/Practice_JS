419709969f3c7085957105512ba13928cf04f722
amrendrakind <vision2010@gmail.com>
2021-10-08 20:32:32 +0530

    Closures Exercise

--- apps/index.js
+++ apps/index.js
@@ -12,3 +12,31 @@ function countdown(number){
   },1000)
 }
 countdown(5)
+// randomGame
+// Write a function called randomGame that selects a random number between 0 and 1 every 1000 milliseconds and each time that a random number is picked, add 1 to a counter. If the number is greater than .75, stop the timer and return the number of tries it took before we found a number greater than .75.
+
+function randomGame(){
+  var count=0, randomNumber
+  var timer=setInterval(function(){
+      randomNumber=Math.random()
+      count++
+      if(randomNumber>0.75){
+          clearInterval(timer)
+          console.log("It took "+count+" try/tries.")
+      }
+  },1000)
+  return "Trying"
+}
+console.log(randomGame())
+
+
+
+
+
+
+
+
+
+
+
+
