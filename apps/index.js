992a2fd7037c62a7bd0fc6feddcc158b0c47a0bc
amrendrakind <vision2010@gmail.com>
2021-10-08 14:51:25 +0530

    Closures

--- apps/index.js
+++ apps/index.js
@@ -30,3 +30,26 @@ console.log("Now age is",ageGrowByOne()) // 29
 console.log("Now age is",ageGrowByOne()) // 30
 console.log("Now age is",ageGrowByOne()) // 31
 console.log("Now age is",ageGrowByOne()) // 32
+/// Another Example-----------
+function createInstructors(){
+  let instructors=["Elie","Matt","Tim"]
+  return{
+    showInstructors: function displayAllInstructors(){
+      return instructors
+    },
+    addInstructors: function addNewInstructor(instructor){
+      instructors.push(instructor)
+      return instructors
+    }
+  }
+}
+console.log("\nInstructor Role\n")
+let firstClass=createInstructors()
+console.log(firstClass)
+console.log(firstClass.showInstructors())
+firstClass.addInstructors("Jennifer")
+console.log(firstClass.showInstructors())
+let secondClass =createInstructors()
+secondClass.addInstructors("Ashley")
+console.log(secondClass.showInstructors())
+console.log(createInstructors().showInstructors())
