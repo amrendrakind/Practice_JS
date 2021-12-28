8964759df3071c95c78729b21247daaaed36b5dc
amrendrakind <vision2010@gmail.com>
2021-12-28 09:40:16 +0530

    Nested Array Exercise

--- apps/NestedArrays.js
+++ apps/NestedArrays.js
@@ -5,3 +5,21 @@ for (let i = 0; i < myFirstSubarray.length; i++) {
         console.log(myFirstSubarray[i][j]);
     }
   }
+
+let arr = [];
+Array.isArray(arr); // true
+Array.isArray("Hello"); // false
+
+function countVowels(arr){
+    let newArr = arr.toString()
+    console.log(newArr)
+}
+
+let nestedArr = [
+  "Elie",
+  ["Matt", ["Tim"]],
+  ["Colt", ["Whiskey", ["Janey"], "Tom"]],
+  "Lorien"
+];
+
+countVowels(nestedArr); // 14
