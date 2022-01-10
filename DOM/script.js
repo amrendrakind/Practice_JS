01689f8d0dfa6f2343434a83f087727482206e9c
amrendrakind <vision2010@gmail.com>
2022-01-10 18:00:45 +0530

    DOM Exercise

--- DOM/script.js
+++ DOM/script.js
@@ -12,5 +12,39 @@ document.addEventListener("DOMContentLoaded", function(){
     var newDiv = document.createElement("div")
     newDiv.className="purple"
     selectSection.appendChild(newDiv)
-    
+
+//////// Race Car
+
+let button = document.querySelector("button")
+let car1 = document.querySelector(".car1")
+let car2 = document.querySelector(".car2")
+car1.style.marginLeft = 0
+car2.style.marginLeft = 0
+
+function reset(car1,car2){
+    clearTimeout(car1.timer)
+    clearTimeout(car2.timer)
+    car1.style.marginLeft = 0
+    car2.style.marginLeft = 0
+    button.disabled = false
+}
+
+button.addEventListener("click", function(event){
+    button.disabled=true
+    car1.timer = setInterval(function (){
+        car1.style.marginLeft = parseInt(car1.style.marginLeft) + Math.random()*60+'px'
+        if(parseInt(car1.style.marginLeft)>window.innerWidth){
+            alert("Car 1 wins!");
+            reset(car1,car2);
+        }
+    }, 60);
+    car2.timer = setInterval(function(){
+        car2.style.marginLeft = parseInt(car2.style.marginLeft) + Math.random()*60 + 'px';
+        if(parseInt(car2.style.marginLeft) > window.innerWidth){
+            alert("Car 2 wins!");
+            reset(car1,car2);
+        }
+    },60)
+})
+
 })
