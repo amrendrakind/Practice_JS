09951c013f104d118338cc5163c30a83cb78b440
amrendrakind <vision2010@gmail.com>
2022-01-10 17:38:46 +0530

    DOM Exercise

--- DOM/script.js
+++ DOM/script.js
@@ -1,4 +1,16 @@
 document.addEventListener("DOMContentLoaded", function(){
     let changeHeading = document.getElementById("change_heading")
     changeHeading.innerText = "Hello World!"
+    
+    let selectSection = document.querySelector("section")
+    selectSection.addEventListener("mouseover", function(event){
+            let selectColor = document.querySelector(".selected")
+            changeHeading.style.color = event.target.className
+            selectColor.innerText = event.target.className.toUpperCase()
+        });
+
+    var newDiv = document.createElement("div")
+    newDiv.className="purple"
+    selectSection.appendChild(newDiv)
+    
 })
