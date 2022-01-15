e9cbe1426f554bfeb1841c1b7661f53e3f2db4f4
amrendrakind <vision2010@gmail.com>
2022-01-15 16:08:41 +0530

    DOM Exercise

--- DOM/script1.js
+++ DOM/script1.js
@@ -2,5 +2,24 @@ window.onload = function(){
 
     let todoForm = document.getElementById("newTodoForm")
     let todoList = document.getElementById("todoList")
-    
+    todoForm.addEventListener("submit", function(event){
+        event.preventDefault()
+        var removeButton = document.createElement("button");
+        removeButton.innerText = "X";
+        var newTodo = document.createElement("li");
+        newTodo.innerText = document.getElementById("task").value;
+        todoList.appendChild(newTodo);
+        newTodo.appendChild(removeButton);
+        todoForm.reset();
+    })
+
+    todoList.addEventListener("click", function(event){
+        if (event.target.tagName.toLowerCase() === 'li') {
+            event.target.style.textDecoration = "line-through";
+        }
+        else if (event.target.tagName.toLowerCase() === 'button') {
+            event.target.parentNode.remove();
+        }
+    })
+
 }
