5a5c5002bf4695d088912c340165e89aef8ed67d
amrendrakind <vision2010@gmail.com>
2022-01-17 00:29:49 +0530

    DOM Exercise

--- DOM/script1.js
+++ DOM/script1.js
@@ -1,27 +1,28 @@
 window.onload = function(){
 
-    // let todoForm = document.getElementById("newTodoForm")
-    // let todoList = document.getElementById("todoList")
-    // todoForm.addEventListener("submit", function(event){
-    //     event.preventDefault()
-    //     var removeButton = document.createElement("button");
-    //     removeButton.innerText = "X";
-    //     var newTodo = document.createElement("li");
-    //     newTodo.innerText = document.getElementById("task").value;
-    //     todoList.appendChild(newTodo);
-    //     newTodo.appendChild(removeButton);
-    //     todoForm.reset();
-    // })
+    let todoForm = document.getElementById("newTodoForm")
+    let todoList = document.getElementById("todoList")
+    todoForm.addEventListener("submit", function(event){
+        event.preventDefault()
+        var removeButton = document.createElement("button");
+        removeButton.innerText = "X";
+        var newTodo = document.createElement("li");
+        newTodo.innerText = document.getElementById("task").value;
+        todoList.appendChild(newTodo);
+        newTodo.appendChild(removeButton);
+        todoForm.reset();
+    })
 
-    // todoList.addEventListener("click", function(event){
-    //     if (event.target.tagName.toLowerCase() === 'li') {
-    //         event.target.style.textDecoration = "line-through";
-    //     }
-    //     else if (event.target.tagName.toLowerCase() === 'button') {
-    //         event.target.parentNode.remove();
-    //     }
-    // })
-
+    todoList.addEventListener("click", function(event){
+        if (event.target.tagName.toLowerCase() === 'li') {
+            event.target.style.textDecoration = "line-through";
+        }
+        else if (event.target.tagName.toLowerCase() === 'button') {
+            event.target.parentNode.remove();
+        }
+    })
+}
+    window.onload = function(){
     var todoForm = document.getElementById("newTodoForm");
     var todoList = document.getElementById("todoList");
 
@@ -70,6 +71,5 @@ window.onload = function(){
             }
         }
     });
-
 
 }
