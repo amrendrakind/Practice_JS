ed5eb1f90bb52961f2b21a13058799ab473bbccb
amrendrakind <vision2010@gmail.com>
2022-01-16 23:27:31 +0530

    DOM Exercise

--- DOM/script1.js
+++ DOM/script1.js
@@ -1,25 +1,75 @@
 window.onload = function(){
 
-    let todoForm = document.getElementById("newTodoForm")
-    let todoList = document.getElementById("todoList")
-    todoForm.addEventListener("submit", function(event){
-        event.preventDefault()
-        var removeButton = document.createElement("button");
-        removeButton.innerText = "X";
+    // let todoForm = document.getElementById("newTodoForm")
+    // let todoList = document.getElementById("todoList")
+    // todoForm.addEventListener("submit", function(event){
+    //     event.preventDefault()
+    //     var removeButton = document.createElement("button");
+    //     removeButton.innerText = "X";
+    //     var newTodo = document.createElement("li");
+    //     newTodo.innerText = document.getElementById("task").value;
+    //     todoList.appendChild(newTodo);
+    //     newTodo.appendChild(removeButton);
+    //     todoForm.reset();
+    // })
+
+    // todoList.addEventListener("click", function(event){
+    //     if (event.target.tagName.toLowerCase() === 'li') {
+    //         event.target.style.textDecoration = "line-through";
+    //     }
+    //     else if (event.target.tagName.toLowerCase() === 'button') {
+    //         event.target.parentNode.remove();
+    //     }
+    // })
+
+    var todoForm = document.getElementById("newTodoForm");
+    var todoList = document.getElementById("todoList");
+
+    // retrieve from localStorage
+    var savedTodos = JSON.parse(localStorage.getItem("todos")) || []
+    for(var i=0; i < savedTodos.length; i++){
         var newTodo = document.createElement("li");
-        newTodo.innerText = document.getElementById("task").value;
+        newTodo.innerText = savedTodos[i].task
+        newTodo.isCompleted = savedTodos[i].isCompleted ? true : false;
+        if(newTodo.isCompleted) {
+            newTodo.style.textDecoration = "line-through";
+        }
         todoList.appendChild(newTodo);
-        newTodo.appendChild(removeButton);
+    }
+
+    todoForm.addEventListener("submit", function(event){
+        event.preventDefault();
+        var newTodo = document.createElement("li");
+        var taskValue = document.getElementById("task").value;
+        newTodo.innerText = taskValue;
+        newTodo.isCompleted = false;
         todoForm.reset();
+        todoList.appendChild(newTodo);
+
+        // save to localStorage
+        savedTodos.push({task: newTodo.innerText, isCompleted: false});
+        localStorage.setItem("todos", JSON.stringify(savedTodos));
     })
 
     todoList.addEventListener("click", function(event){
-        if (event.target.tagName.toLowerCase() === 'li') {
-            event.target.style.textDecoration = "line-through";
+        var clickedListItem = event.target;
+
+        if(!clickedListItem.isCompleted) {
+            clickedListItem.style.textDecoration = "line-through";
+            clickedListItem.isCompleted = true;
+        } else {
+            clickedListItem.style.textDecoration = "none";
+            clickedListItem.isCompleted = false;
         }
-        else if (event.target.tagName.toLowerCase() === 'button') {
-            event.target.parentNode.remove();
+
+        // breaks for duplicates - another option is to have dynamic IDs
+        for(var i=0; i < savedTodos.length; i++){
+            if(savedTodos[i].task === clickedListItem.innerText){
+                savedTodos[i].isCompleted = clickedListItem.isCompleted;
+                localStorage.setItem("todos", JSON.stringify(savedTodos));
+            }
         }
-    })
+    });
+
 
 }
