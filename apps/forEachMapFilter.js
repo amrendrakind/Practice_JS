182406efa8d3367b01eaadbeb5a0a4cbc4c231df
amrendrakind <vision2010@gmail.com>
2022-01-17 13:09:47 +0530

    JS Iterator Exercise

--- apps/forEachMapFilter.js
+++ apps/forEachMapFilter.js
@@ -6,7 +6,7 @@ arr.forEach(function(val,index,arr){
 arr.forEach(function(val){
     console.log(val)
 })
-
+//rite a function called double which accepts an array. The function should return a new array with all of the values doubled.
 function doubleVal(arr){
     let doubleArray = []
     arr.forEach(function(val){
@@ -14,4 +14,43 @@ function doubleVal(arr){
     })
     return doubleArray
 }
-console.log(doubleVal(arr))
+console.log(doubleVal(arr));
+//Write a function called printFirstAndLast which accepts an array (of objects) and console.logs a new string with the first character and the last character of each value.
+////printFirstAndLast(['awesome','example','of','forEach'])
+function printFirstAndLast(arr){
+    let temp =[];
+    arr.forEach(function(val){
+        let len =val.length
+        console.log(val[0]+val[len-1])
+    })
+}
+
+printFirstAndLast(['awesome','example','of','forEach'])
+
+//Write a function called addKeyAndValue which accepts three parameters, an array (of objects), a key and a value. This function should return the array of objects after each key and value have been added to each object in the array.
+
+function addKeyAndValue(arrObj,key,val){
+    let objVal=[]
+    arrObj.forEach(function (obj){
+        obj[key]=val
+        objVal.push(obj)
+    })
+    return objVal
+}
+console.log(addKeyAndValue([{name: 'Elie'},{name: 'Tim'},{name: 'Elie'}], "isInstructor", true))
+
+///////Map
+
+function doubleArray(arr){
+   return arr.map(function(val){
+        return val*2
+    })
+}
+arr = [1,2,3,4,5]
+
+console.log(doubleArray(arr))
+let doubleval = arr.map(function(val){ return val*2})
+let tripple = arr.map(function(val){ return val*3 })
+console.log(doubleval)
+console.log(tripple)
+
---
+++ apps/forEachMapFilterExercise.js
@@ -0,0 +1,2 @@
+//Write a function called valTimesIndex which accepts an array of numbers and returns a new array with each value multiplied by the index it is at in the array:
+
